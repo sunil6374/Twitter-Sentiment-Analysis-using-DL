64333b885957bdd2457444ac9dbdbead8bb7c3da
sunil6374 <sunil6374@rediffmail.com>
2026-06-28 21:29:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import numpy as np
 
 model=load_model('mode.h5')
 
-with open ('Tokenizer.pkl','rb') as file:
+with open ('tokenizer.pkl','rb') as file:
     tokenizer=pickle.load(file)
     
 st.title('Twitter Tweets Sentimental Analysis')
